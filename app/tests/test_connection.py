dc1c0d6564ad134e96853d71957ed73fb7d266a3
memorisanka <j.krawczyk87@gmail.com>
2022-11-01 16:20:44 +0100

    tests: change directory

--- app/tests/test_connection.py
+++ app/tests/test_connection.py
@@ -12,7 +12,6 @@ class TestAPI(unittest.TestCase):
 
         resp = requests.get(self.url)
         self.assertEqual(resp.status_code, 401)
-        print('Test 1 completed')
 
     def test_should_return_200(self):
         """Test should return 200, because url is valid"""
@@ -21,27 +20,20 @@ class TestAPI(unittest.TestCase):
         url = f"{self.url}{self.api_key}&q={localization}"
         resp = requests.get(url)
         self.assertEqual(resp.status_code, 200)
-        print('Test 2 completed')
 
     def test_should_return_true(self):
         """Test should return True, because there is such city as Berlin."""
 
         conn = ConnectionMixin('Berlin')
         self.assertTrue(conn.connect_to_api())
-        print('Test 3 completed')
 
     def test_should_return_false(self):
         """Test should return False, because there is no such city as ababcd."""
 
         conn = ConnectionMixin('ababcd')
         self.assertFalse(conn.connect_to_api())
-        print('Test 4 completed')
 
 
 if __name__ == '__main__':
     test = TestAPI()
 
-    test.test_should_return_401()
-    test.test_should_return_200()
-    test.test_should_return_true()
-    test.test_should_return_false()
